4ebb21e2b4beb579232bdd0256033701d866a324
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2023-02-15 16:48:42 +0600

    Initial commit

---
+++ app/src/main/java/edu/ewubd/a2019_3_60_050calculator/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.ewubd.a2019_3_60_050calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
